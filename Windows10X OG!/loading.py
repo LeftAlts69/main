c9eaadfbfc3bbac7cbf6c1d475044d628dee9a34
LeftAlts69 <mki12826120@gmail.com>
2025-09-27 22:01:59 +0530

    Update loading.py

--- Windows10X OG!/loading.py
+++ Windows10X OG!/loading.py
@@ -1,10 +1,11 @@
 import time
 import sys
 
-spinner = ["|", "/", "-", "\\"]
+spinner = ["x", "+"]
 
 while True:
     for frame in spinner:
         sys.stdout.write("\rLoading " + frame)
         sys.stdout.flush()
         time.sleep(0.1)
+
